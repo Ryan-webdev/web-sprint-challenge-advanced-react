7aab60af034617c5028a42492f8aecec87fad011
Ryan Schueck <ryschueck@gmail.com>
2020-06-12 19:26:10 -0400

    finished useForm hook and plant list. moving onto tests

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {container, getByText} = render(<CheckoutForm />);
+
+    const header = container.querySelector('h2')
+    expect(header.textContent).toBe('Checkout Form')
+});
 
 test("form shows success message on submit with form details", () => {});
